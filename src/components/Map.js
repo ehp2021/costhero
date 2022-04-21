a99e5262cf22caff3187b5005a01da7424691bad
ehp2021 <emmahpaik@gmail.com>
2022-04-21 16:38:03 -0500

    pop up survey working

--- src/components/Map.js
+++ src/components/Map.js
@@ -73,7 +73,9 @@ function Map(props) {
       {/* <LoadScript 
         googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} 
       > */}
-        <Typography variant="h6" style={{marginBottom: '10px', color: '#222286'}}>Search and compare 427 real LASIK cost quotes from real patients from around the globe</Typography>
+      <Box style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+        <Typography variant="h5" style={{marginBottom: '10px', color: '#222286'}}>Search and compare 427 real LASIK cost quotes from real patients from around the globe</Typography>
+      </Box>  
         <Typography variant="body1">How Much Does Lasik Eye Surgery Cost Near Me? </Typography>
 
         <Search  goToMap={goToMap} />

a5c5ce060d94a6be0698c7465914ce32e6590093
ehp2021 <emmahpaik@gmail.com>
2022-04-21 17:40:30 -0500

    sizing for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -118,10 +118,10 @@ function Header(props) {
           </Box>
         
           {/* <Typography variant="body2" sx={{flexGrow: 0.1, color: '#222286'}}>Compare Lasik Costs From Real Patients</Typography> */}
-          <Typography sx={{flexGrow: 0.1, color: "#000"}}><Link to="/about">ABOUT</Link></Typography>
-          <Typography sx={{flexGrow: 0.1, color: "#000"}}><a href="https://tally.so#tally-open=mKdag3">CONTACT US</a></Typography>
-          <Typography sx={{flexGrow: 0.1, color: "#000"}}><a href="https://tally.so#tally-open=n0NK03&tally-layout=modal&tally-align-left=1">SIGN-UP</a></Typography>
-          <Typography sx={{flexGrow: 0.1, color: "#000"}}><a href="https://tally.so#tally-open=mREa9n&tally-layout=modal&tally-width=750&tally-auto-close=0">FEEDBACK</a></Typography>
+          <Typography xs={4} md={2} sx={{flexGrow: 0.1, color: "#000"}}><Link to="/about">ABOUT</Link></Typography>
+          <Typography xs={4} md={2} sx={{flexGrow: 0.1, color: "#000"}}><a href="https://tally.so#tally-open=mKdag3">CONTACT US</a></Typography>
+          <Typography xs={4} md={2} sx={{flexGrow: 0.1, color: "#000"}}><a href="https://tally.so#tally-open=n0NK03&tally-layout=modal&tally-align-left=1">SIGN-UP</a></Typography>
+          <Typography xs={4} md={2} sx={{flexGrow: 0.1, color: "#000"}}><a href="https://tally.so#tally-open=mREa9n&tally-layout=modal&tally-width=750&tally-auto-close=0">FEEDBACK</a></Typography>
           {/* <Typography sx={{flexGrow: 0.1, color: "#fff"}}>Email</Typography> */}
 
           <Box>

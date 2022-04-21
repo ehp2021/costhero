0cd37d70505daaa845bc4560e93695ab1ea07a5d
ehp2021 <emmahpaik@gmail.com>
2022-04-21 18:23:01 -0500

    about page edit

--- src/components/About.js
+++ src/components/About.js
@@ -15,9 +15,20 @@ function About (){
     <div
       style={{marginTop: '100px' }}
     >
-      <Box >
+      <Box style={{margin: '10px'}}>
         <Typography variant="h5">
         Find out what others are paying for LASIK in your city.
+        </Typography>
+      </Box>
+
+      <Box style={{margin: '10px'}}>
+        <Typography variant="h6">
+          Why did you build this? What can I do on this website?
+          </Typography>
+        <Typography variant="body1">
+          Our site allows users to compare LASIK providers and costs in your area without doing multiple consultations. 
+          We built this to help potential LASIK patients find out the Lasik surgery prices in their area. All of the costs and 
+          comments come from real patients who have gotten LASIK. We are adding more and more data every day!
         </Typography>
       </Box>
 
@@ -42,16 +53,6 @@ function About (){
         Radial Keratotomy or RK and Photorefractive Keratectomy or PRK are other refractive surgeries used to reshape the cornea. In RK, a very sharp knife is used to cut slits in the cornea changing its shape. PRK was the first surgical procedure developed to reshape the cornea, by sculpting, using a laser. Later, LASIK was developed. The same type of laser is used for LASIK and PRK. Often the exact same laser is used for the two types of surgery. The major difference between the two surgeries is the way that the stroma, the middle layer of the cornea, is exposed before it is vaporized with the laser. In PRK, the top layer of the cornea, called the epithelium, is scraped away to expose the stromal layer underneath. In LASIK, a flap is cut in the stromal layer and the flap is folded back.
         </Typography>
         <Typography variant="caption">Source: FDA</Typography>
-      </Box>
-
-      <Box style={{margin: '10px'}}>
-        <Typography variant="h6">
-          Why did you build this? What can I do on this website?
-          </Typography>
-        <Typography variant="body1">
-          Our site allows users to compare Lasik providers and costs in your area without doing multiple consultations. 
-          We built this to help potential Lasik patients find out the Lasik surgery prices in their area.
-        </Typography>
       </Box>
       
       <Box>
